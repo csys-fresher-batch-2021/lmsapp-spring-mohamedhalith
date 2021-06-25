ff0e74e34ea9312d6ebe220e23ac2d983c93494d
MohamedHalith001 <mohamedhalith1994@gmail.com>
2021-06-25 09:34:46 +0530

    Task : Implement Spring Scheduler

--- src/main/java/in/mohamedhalith/LmsApplication.java
+++ src/main/java/in/mohamedhalith/LmsApplication.java
@@ -2,11 +2,13 @@ package in.mohamedhalith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableScheduling
 public class LmsApplication {
 	
 	public static void main(String[] args) {
